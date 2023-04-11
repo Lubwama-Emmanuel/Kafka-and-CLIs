60b7ec8ca42040324e45e6020420b958bde4d3d0
Rex Munil <lubwamaemmanuel1@gmail.com>
2023-04-12 00:13:57 +0300

    refactor:(alittle fmting)

--- cmd/root.go
+++ cmd/root.go
@@ -21,22 +21,12 @@ to quickly create a Cobra application.`,
 	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		os.Exit(1)
 	}
 }
 
-func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.Kafka-and-CLIs.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
+func RootInit() {
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
