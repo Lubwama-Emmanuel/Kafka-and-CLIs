81b6f56520e3cf70f4924b9d71053ecb204bbf2f
Rex Munil <lubwamaemmanuel1@gmail.com>
2023-04-12 00:15:12 +0300

    refactor:(commenting and descs)

--- consumers/consumer.go
+++ consumers/consumer.go
@@ -2,38 +2,36 @@ package consumers
 
 import (
 	"fmt"
-	"time"
 
 	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 	log "github.com/sirupsen/logrus"
 )
 
-func Consumer(topic, server, from string) {
+func Consumer(topic, server, from string) error {
 	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
 		"bootstrap.servers": server,
 		"group.id":          "myGroup",
 		"auto.offset.reset": from,
 	})
 	if err != nil {
-		panic(err)
+		return fmt.Errorf("error creating consumer %w", err)
 	}
 
-	if err := consumer.SubscribeTopics([]string{"development"}, nil); err != nil {
-		log.Error("An error occurred", err)
+	subErr := consumer.SubscribeTopics([]string{topic}, nil)
+	if subErr != nil {
+		return fmt.Errorf("error subscribing to topics %w", subErr)
 	}
 
-	// A signal handler or similar could be used to set this to false to break the loop.
 	run := true
-
 	for run {
-		msg, err := consumer.ReadMessage(time.Second)
-		if err == nil {
-			log.Printf("Message on %s: %s\n", msg.TopicPartition, string(msg.Value))
-		} else if !err.(kafka.Error).IsTimeout() {
-			errorMessage := fmt.Sprintf("Consumer error: %v (%v)\n", err, msg)
-			log.Error(errorMessage)
+		msg, err := consumer.ReadMessage(-1)
+		if err != nil {
+			consumer.Close()
+			return fmt.Errorf("an error occurred %w", err)
 		}
+
+		log.Printf("Message on %s: %s\n", msg.TopicPartition, string(msg.Value))
 	}
 
-	consumer.Close()
+	return nil
 }
