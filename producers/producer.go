e07dfcef5f59a77c44263c2658d7dbdfccbaaf0b
Rex Munil <lubwamaemmanuel1@gmail.com>
2023-04-12 00:15:31 +0300

    refactor:(commenting and descs)

--- producers/producer.go
+++ producers/producer.go
@@ -1,25 +1,23 @@
 package producers
 
 import (
+	"fmt"
+
 	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 	log "github.com/sirupsen/logrus"
 )
 
-func Producer(topic, server, message string) {
-	producer, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": server})
-	if err != nil {
-		log.Panic(err)
-	}
+func Producer(topic, server, message string) error {
+	producer, _ := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": server})
 
 	defer producer.Close()
 
 	// Delivery report handler for produced messages.
 	go func() {
 		for e := range producer.Events() {
-			switch ev := e.(type) {
-			case *kafka.Message:
+			if ev, ok := e.(*kafka.Message); ok {
 				if ev.TopicPartition.Error != nil {
-					log.Printf("Delivery failed: %v\n", ev.TopicPartition)
+					log.Error("Delivery failed:", ev.TopicPartition.Error)
 				} else {
 					log.Printf("Message to %v\n", ev.TopicPartition)
 				}
@@ -32,9 +30,10 @@ func Producer(topic, server, message string) {
 		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
 		Value:          []byte(message),
 	}, nil); err != nil {
-		log.Error("An error occurred", err)
+		return fmt.Errorf("an error occurred producing messages %w", err)
 	}
-
 	// Wait for message deliveries before shutting down.
 	producer.Flush(15 * 1000)
+
+	return nil
 }
