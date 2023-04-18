22a1326f34f53d2bab224ceed54f57caf0f9d167
Rex Munil <lubwamaemmanuel1@gmail.com>
2023-04-18 16:23:59 +0300

    feat:(implementing consumer with interfaces)

--- consumers/consumer.go
+++ consumers/consumer.go
@@ -1,37 +1,25 @@
 package consumers
 
 import (
-	"fmt"
-
-	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 	log "github.com/sirupsen/logrus"
 )
 
-func Consumer(topic, server, from string) error {
-	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
-		"bootstrap.servers": server,
-		"group.id":          "myGroup",
-		"auto.offset.reset": from,
-	})
-	if err != nil {
-		return fmt.Errorf("error creating consumer %w", err)
-	}
+func (c *KafkaConsumer) ConsumeMessages(topic string) {
 
-	subErr := consumer.SubscribeTopics([]string{topic}, nil)
+	subErr := c.Consumer.Subscribe(topic, nil)
 	if subErr != nil {
-		return fmt.Errorf("error subscribing to topics %w", subErr)
+		log.Error("an error ocurred", subErr)
 	}
 
 	run := true
 	for run {
-		msg, err := consumer.ReadMessage(-1)
+		msg, err := c.Consumer.ReadMessage(-1)
 		if err != nil {
-			consumer.Close()
-			return fmt.Errorf("an error occurred %w", err)
+			c.Consumer.Close()
+			log.Error("an error ocurred", err)
 		}
 
 		log.Printf("Message on %s: %s\n", msg.TopicPartition, string(msg.Value))
 	}
 
-	return nil
 }
