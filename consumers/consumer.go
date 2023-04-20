f2ffb3bdb93f682751b3049eadf305c1451d2038
Rex Munil <lubwamaemmanuel1@gmail.com>
2023-04-20 11:51:48 +0300

    feat:(added interface implementation)

--- consumers/consumer.go
+++ consumers/consumer.go
@@ -1,23 +1,55 @@
 package consumers
 
 import (
+	"time"
+
 	log "github.com/sirupsen/logrus"
+
+	"github.com/Lubwama-Emmanuel/Kafka-and-CLIs/models"
 )
 
-func (c *KafkaConsumer) ConsumeMessages(topic string) {
-	subErr := c.Consumer.Subscribe(topic, nil)
+type Provider interface {
+	Subscribe(topic string) error
+	ReadMessage(time.Duration) (models.Message, error)
+	Close() error
+}
+
+type Consumer struct {
+	provider Provider
+	run      bool
+}
+
+type ConsumerConfig struct {
+	Server string
+	From   string
+	Group  string
+}
+
+func NewConsumer(provider Provider) *Consumer {
+	return &Consumer{provider: provider}
+}
+
+func (c *Consumer) ConsumeMessages(topic string) {
+	subErr := c.provider.Subscribe(topic)
 	if subErr != nil {
-		log.Error("an error occurred", subErr)
+		log.Error("subscription error", subErr)
 	}
 
-	run := StartConsumer()
-	for run {
-		msg, err := c.Consumer.ReadMessage(-1)
+	for c.run {
+		msg, err := c.provider.ReadMessage(-1)
 		if err != nil {
-			c.Consumer.Close()
-			log.Error("an error occurred", err)
+			c.provider.Close()
+			log.Error("read messages error", err)
 		}
 
-		log.Printf("Message on %s: %s\n", msg.TopicPartition, string(msg.Value))
+		log.Printf("Message: %s\n", string(msg.Value))
 	}
+}
+
+func (c *Consumer) StartConsumer() {
+	c.run = true
+}
+
+func (c *Consumer) StopConsumer() {
+	c.run = false
 }
