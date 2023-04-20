3fb2cf29ff9da230f431ba4209762e0bff861bf0
Rex Munil <lubwamaemmanuel1@gmail.com>
2023-04-20 13:10:23 +0300

    test:(consumer test file)

---
+++ consumers/consumer_test.go
@@ -0,0 +1,98 @@
+package consumers_test
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/Lubwama-Emmanuel/Kafka-and-CLIs/consumers"
+	"github.com/Lubwama-Emmanuel/Kafka-and-CLIs/consumers/mocks"
+	"github.com/Lubwama-Emmanuel/Kafka-and-CLIs/models"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConsumeMessages(t *testing.T) {
+	t.Parallel()
+
+	type args struct {
+		topic string
+	}
+
+	type fields struct {
+		provider *mocks.MockProvider
+	}
+
+	tests := []struct {
+		testName string
+		args     args
+		prepare  func(t *testing.T, f *fields)
+		wantErr  assert.ErrorAssertionFunc
+	}{
+		{
+			testName: "success",
+			args: args{
+				topic: "test_topic",
+			},
+			prepare: func(t *testing.T, f *fields) {
+				f.provider.EXPECT().Subscribe("test_topic").Return(nil)
+
+				f.provider.EXPECT().ReadMessage(time.Millisecond*100).Return(models.Message{}, nil)
+			},
+			wantErr: assert.NoError,
+		},
+		{
+			testName: "error/subscribe",
+			args: args{
+				topic: "test_topic",
+			},
+			prepare: func(t *testing.T, f *fields) {
+				f.provider.EXPECT().Subscribe(gomock.Any()).Return(assert.AnError)
+
+			},
+			wantErr: assert.Error,
+		},
+		{
+			testName: "error/read",
+			args: args{
+				topic: "test_topic",
+			},
+			prepare: func(t *testing.T, f *fields) {
+				f.provider.EXPECT().Subscribe(gomock.Any()).Return(nil)
+
+				f.provider.EXPECT().ReadMessage(gomock.Any()).Return(models.Message{}, assert.AnError)
+
+			},
+			wantErr: assert.Error,
+		},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.testName, func(t *testing.T) {
+			t.Parallel()
+			ctrl := gomock.NewController(t)
+
+			f := fields{
+				provider: mocks.NewMockProvider(ctrl),
+			}
+
+			if tc.prepare != nil {
+				tc.prepare(t, &f)
+			}
+
+			consumer := consumers.NewConsumer(f.provider)
+			consumer.StartConsumer()
+
+			err := consumer.ConsumeMessages(tc.args.topic)
+			if err != nil && tc.wantErr == nil {
+				assert.Fail(t, fmt.Sprintf("Test %v Error not expected but got one:\n"+"error: %q", tc.testName, err))
+				return
+			}
+
+			if !tc.wantErr(t, err, fmt.Sprintf("Consume(%v)", tc.args.topic)) {
+				return
+			}
+		})
+	}
+}
