711832c460434cd99dbe0c80ed1951b955fa5607
Rex Munil <lubwamaemmanuel1@gmail.com>
2023-04-21 11:39:17 +0300

    feat:(models for messages)

--- models/models.go
+++ models/models.go
@@ -1,5 +1,11 @@
 package models
 
+import "github.com/confluentinc/confluent-kafka-go/v2/kafka"
+
 type Message struct {
 	Value []byte
+}
+
+type Kmessage struct {
+	TopicPartition kafka.TopicPartition
 }
