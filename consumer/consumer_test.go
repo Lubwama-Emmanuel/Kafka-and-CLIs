d1f27b81c636bf36bfb4305da3367746029b86d5
Rex Munil <lubwamaemmanuel1@gmail.com>
2023-04-27 16:26:53 +0300

    test:(improved test coverage)

--- consumer/consumer_test.go
+++ consumer/consumer_test.go
@@ -5,12 +5,19 @@ import (
 	"testing"
 	"time"
 
+	"github.com/Lubwama-Emmanuel/Kafka-and-CLIs/config"
 	"github.com/Lubwama-Emmanuel/Kafka-and-CLIs/consumer"
 	"github.com/Lubwama-Emmanuel/Kafka-and-CLIs/consumer/mocks"
 	"github.com/Lubwama-Emmanuel/Kafka-and-CLIs/models"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 )
+
+var configs = config.ProviderConfig{
+	Server: "server",
+	Group:  "group",
+	From:   "from",
+}
 
 func TestConsumeMessages(t *testing.T) {
 	t.Parallel()
@@ -35,6 +42,8 @@ func TestConsumeMessages(t *testing.T) {
 				topic: "test_topic",
 			},
 			prepare: func(t *testing.T, f *fields) {
+				f.provider.EXPECT().SetUp(configs).Return(nil)
+				
 				f.provider.EXPECT().Subscribe("test_topic").Return(nil)
 
 				f.provider.EXPECT().ReadMessage(time.Millisecond*100).Return(models.Message{}, nil)
@@ -47,6 +56,8 @@ func TestConsumeMessages(t *testing.T) {
 				topic: "test_topic",
 			},
 			prepare: func(t *testing.T, f *fields) {
+				f.provider.EXPECT().SetUp(configs).Return(nil)
+
 				f.provider.EXPECT().Subscribe(gomock.Any()).Return(assert.AnError)
 			},
 			wantErr: assert.Error,
@@ -57,11 +68,25 @@ func TestConsumeMessages(t *testing.T) {
 				topic: "test_topic",
 			},
 			prepare: func(t *testing.T, f *fields) {
+				f.provider.EXPECT().SetUp(configs).Return(nil)
+
 				f.provider.EXPECT().Subscribe(gomock.Any()).Return(nil)
 
 				f.provider.EXPECT().ReadMessage(gomock.Any()).Return(models.Message{}, assert.AnError)
 
 				f.provider.EXPECT().Close().Return(nil)
+			},
+			wantErr: assert.Error,
+		},
+		{
+			testName: "error/read",
+			args: args{
+				topic: "test_topic",
+			},
+			prepare: func(t *testing.T, f *fields) {
+				f.provider.EXPECT().SetUp(gomock.Any()).Return(assert.AnError)
+
+				f.provider.EXPECT().Subscribe(gomock.Any()).Return(assert.AnError)
 			},
 			wantErr: assert.Error,
 		},
@@ -83,6 +108,11 @@ func TestConsumeMessages(t *testing.T) {
 
 			consumer := consumer.NewConsumer(f.provider)
 			consumer.StartConsumer()
+			setupErr := consumer.SetUpProvider(configs)
+			if setupErr != nil && tc.wantErr == nil {
+				assert.Fail(t, fmt.Sprintf("Test %v Error not expected but got one:\n"+"error: %q", tc.testName, setupErr))
+				return
+			}
 
 			err := consumer.ConsumeMessages(tc.args.topic)
 			if err != nil && tc.wantErr == nil {
